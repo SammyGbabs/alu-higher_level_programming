fa7198dd95005360133d12ce167888d4d13eb47e
SammyGbabs <s.babalola@alustudent.com>
2023-02-28 03:36:23 -0800

    2-safe_print_list_integers.py

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def safe_print_list_integers(my_list=[], x=0):
     """Print the first x elements of a list that are integers.
 
@@ -19,4 +20,3 @@ def safe_print_list_integers(my_list=[], x=0):
             continue
     print("")
     return (ret)
-
