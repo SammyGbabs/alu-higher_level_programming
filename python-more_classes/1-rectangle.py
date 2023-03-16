666cc6e6e36baeb3859103d769e3083d65936004
SammyGbabs <s.babalola@alustudent.com>
2023-03-16 04:39:28 -0700

    1-rectangle.py

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Define a class Rectangle."""
+
+
+class Rectangle:
+    """Represent a rectangle."""
+
+    def __init__(self, size):
+        """Initialize a new Rectangle.
+
+        Args:
+            size (int): The size of the new rectangle.
+        """
+        self.__size = size
