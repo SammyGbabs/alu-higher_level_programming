60dc69924ddeaeac6e3bd20c46b3e25e366ab9d7
SammyGbabs <s.babalola@alustudent.com>
2023-06-14 17:52:04 -0700

    7-script.js

--- javascript-web_jquery/7-script.js
+++ javascript-web_jquery/7-script.js
@@ -1,4 +1,4 @@
 // JS to fetches and replace character with URL
-$.get('https://swapi.co/api/people/5/?format=json', function (data) {
+$.get('https://swapi-api.alx-tools.com/api/people/5/?format=json', function (data) {
   $('#character').text(data.name);
 });
